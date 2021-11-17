e71e93d356315fd41b2704176cf67154893c24d0
synthline <mso.accts.af@outlook.com>
2021-11-17 07:25:48 +0100

    2

---
+++ python_intro_prog/handin2redo/Student.py
@@ -0,0 +1,18 @@
+class Student:
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+stud1 = Student("Alex",32)
+
+def main():
+    print(stud1.age) 
+    print(stud1.name) 
+    print("hahadh")
+     
+
+if __name__ == '__main__':
+    main()      
+   
+   
