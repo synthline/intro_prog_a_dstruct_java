0b60fcc9d1067f44c10ce07931797f5200307f1d
synthline <mso.accts.af@outlook.com>
2021-11-22 10:58:34 +0100

    4

---
+++ algo_and_dstruct/stdCodeBook/Lecture3/Ex3_1.java
@@ -0,0 +1,35 @@
+public class Ex3_1 {
+
+   private String[] s;
+   private String[] t;
+
+   public Ex3_1(String[]S, String[]T) {
+
+      this.s = S;
+      this.t = T;
+   }
+
+//    public void check(String[]s, String[]t) {
+//       int count = 0;
+//        for (i=0 ; i < String[]s.size(); i++) {
+//           for (j=0; j < String[]t.size(); j++) {
+//              if (S[i] == T[j]) {
+//                count ++;
+//              }
+//           }
+//        }
+//       if (count == String[]t.size()) {
+//          System.out.println("It's a match baby!");
+//       }
+//        }
+
+//  public static void main(String[] args) {
+    
+//     S = "[C,T,G,A,C,C];
+//     T = "TGACCAC";
+
+//    check(S,T);
+
+//  }
+
+}
